d4201c23605f8989c3a54478dc04eb789928f210
manu <a@b.c>
2023-08-16 14:39:57 +0200

    add the typical missing stuff

--- driver/flash.c
+++ driver/flash.c
@@ -1,5 +1,7 @@
 /* Copyright 2023 Dual Tachyon
  * https://github.com/DualTachyon
+ * Copyright 2023 Manuel Jedinger
+ * https://github.com/manujedi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
