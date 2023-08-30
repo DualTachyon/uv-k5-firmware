35400975600741a477bb6eff47f0686d7f3d3e8d
Dual Tachyon <N/A>
2023-08-30 15:56:18 +0100

    Fixed the gain in BK4819_TransmitTone.

--- driver/bk4819.c
+++ driver/bk4819.c
@@ -687,7 +687,7 @@ void BK4819_TransmitTone(bool bLocalLoopback, uint32_t Frequency)
 	BK4819_EnterTxMute();
 	BK4819_WriteRegister(BK4819_REG_70, 0
 			| BK4819_REG_70_MASK_ENABLE_TONE1
-			| (64U << BK4819_REG_70_SHIFT_TONE1_TUNING_GAIN));
+			| (96U << BK4819_REG_70_SHIFT_TONE1_TUNING_GAIN));
 	BK4819_WriteRegister(BK4819_REG_71, (uint16_t)(Frequency * 10.32444));
 	if (bLocalLoopback) {
 		BK4819_SetAF(BK4819_AF_BEEP);
