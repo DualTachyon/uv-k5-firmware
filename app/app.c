632fd3ef980685718c5ce2145c30f2219778a028
Dual Tachyon <N/A>
2023-09-15 21:36:10 +0100

    Fixed step 2/3 of 8.33kHz.

--- app/app.c
+++ app/app.c
@@ -386,8 +386,12 @@ void APP_SetFrequencyByStep(VFO_Info_t *pInfo, int8_t Step)
 	if (pInfo->StepFrequency == 833) {
 		const uint32_t Lower = LowerLimitFrequencyBandTable[pInfo->Band];
 		const uint32_t Delta = Frequency - Lower;
-		const uint32_t Base = (Delta / 2500) * 2500;
+		uint32_t Base = (Delta / 2500) * 2500;
 		const uint32_t Index = ((Delta - Base) % 2500) / 833;
+
+		if (Index == 2) {
+			Base++;
+		}
 
 		Frequency = Lower + Base + (Index * 833);
 	}
